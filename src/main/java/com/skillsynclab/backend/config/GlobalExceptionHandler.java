76cdf9472ee6e81b22f9d376166cbda37b349b4e
akash-de-alwis <akashdealwis@gmail.com>
2025-04-27 08:48:32 +0530

    security done.

--- src/main/java/com/skillsynclab/backend/config/GlobalExceptionHandler.java
+++ src/main/java/com/skillsynclab/backend/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.skillsynclab.backend.config;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
--- src/main/java/com/skillsynclab/backend/config/SecurityConfig.java
+++ src/main/java/com/skillsynclab/backend/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
