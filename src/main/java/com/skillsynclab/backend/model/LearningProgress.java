99b5939a06cfcc8e22e9f44c52affca7a371d80e
Amanda Vihangani <163232853+IT22081452@users.noreply.github.com>
2025-05-03 11:42:58 +0530

    getfuntion updated

--- src/main/java/com/skillsynclab/backend/model/LearningProgress.java
+++ src/main/java/com/skillsynclab/backend/model/LearningProgress.java
@@ -28,13 +28,16 @@ public class LearningProgress {
         return id;
     }
 
+
     public void setId(String id) {
         this.id = id;
     }
 
+
     public String getTitle() {
         return title;
     }
+
 
     public void setTitle(String title) {
         this.title = title;
