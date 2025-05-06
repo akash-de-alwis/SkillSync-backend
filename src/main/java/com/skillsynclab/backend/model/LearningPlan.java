738318d8b863dce27aa64f50fcfd7793799b6eb5
it22054722 <it22054722@my.sliit.lk>
2025-05-06 15:32:46 +0530

    learning plans java update 1.5

--- src/main/java/com/skillsynclab/backend/model/LearningPlan.java
+++ src/main/java/com/skillsynclab/backend/model/LearningPlan.java
@@ -68,7 +68,6 @@ public class LearningPlan {
     public String getDuration() {
         return duration;
     }
-
     public void setDuration(String duration) {
         this.duration = duration;
     }
