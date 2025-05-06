a1f9fb5bbcf2c5de68f7e3ca19b309e4e296b975
it22054722 <it22054722@my.sliit.lk>
2025-05-06 11:31:57 +0530

    learning plans service update 1.2

--- src/main/java/com/skillsynclab/backend/service/LearningPlanService.java
+++ src/main/java/com/skillsynclab/backend/service/LearningPlanService.java
@@ -28,7 +28,6 @@ public class LearningPlanService {
         plan.setCreatedAt(LocalDateTime.now().toString());
         plan.setFollowers(0);
         return learningPlanRepository.save(plan);
-
     }
 
     public LearningPlan updatePlan(String id, LearningPlan plan) {
